becc2a4098be249cd8e4309db05d43e4847a0167
Vadym Bihun <bihunva@gmail.com>
2023-04-24 18:01:35 +0300

    Optimize Borrowing query with select_related

--- borrowings/views.py
+++ borrowings/views.py
@@ -10,13 +10,14 @@ class BorrowingViewSet(
     mixins.RetrieveModelMixin,
     viewsets.GenericViewSet
 ):
-    queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
 
     def get_queryset(self):
         if self.request.user.is_superuser:
-            return Borrowing.objects.all()
-        return Borrowing.objects.filter(user=self.request.user)
+            return Borrowing.objects.all().select_related("book")
+        return Borrowing.objects.filter(
+            user=self.request.user
+        ).select_related("book")
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)

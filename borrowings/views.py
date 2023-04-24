f10f9c198054405a05c3afd821e4ecdde049b583
Vadym Bihun <bihunva@gmail.com>
2023-04-24 17:46:36 +0300

    Add BorrovingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

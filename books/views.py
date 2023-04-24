5c9fc7c890e58b7436d5566f823ddb6ad217ab23
oksana-feshchenko <foksanv@gmail.com>
2023-04-24 16:36:46 +0300

    create view for books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from books.models import Book
+from books.serializers import BookSerializer
 
-# Create your views here.
+
+class BookViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+
+
+

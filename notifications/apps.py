848a67b9ea4123e673c998db020e68722f48872b
Vadym Bihun <bihunva@gmail.com>
2023-04-24 14:27:49 +0300

    Fixed typo in notifications/apps.py

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NitificationsConfig(AppConfig):
+class NotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "nitifications"
+    name = "notifications"

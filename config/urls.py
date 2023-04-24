7f7537a99ec89a3e8deb2a278bfcaef21f23621d
Vadym Bihun <bihunva@gmail.com>
2023-04-24 17:47:41 +0300

    Add router for BorrowingViewSet

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls", namespace="users")),
+    path(
+        "api/borrowings/",
+        include("borrowings.urls", namespace="borrowings")
+    ),
 ]

398879a7afdbd349a6a2fdb86072e29ebf020e54
oksana-feshchenko <foksanv@gmail.com>
2023-04-24 16:54:59 +0300

    add permissions

--- books/views.py
+++ books/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+from books.permissions import IsAdminOrReadOnly
+
+
+class BookViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = (IsAdminOrReadOnly,)
+
+    def get_permissions(self):
+        if self.action == "list":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminOrReadOnly]
+        return [permission() for permission in permission_classes]

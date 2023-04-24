6e73ee36bc7722c7e11fd7812c5e3da8c07bbe2b
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-04-24 16:37:29 +0200

    Add Payment Viewset, with filering by user if not staff

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if not user.is_staff:
+            return self.queryset.filter(borrowing__user=user)
+
+        return self.queryset

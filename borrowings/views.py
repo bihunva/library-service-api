71cfb58eb5e6dfdd434c03ce52accd3e0b096772
Danylo <danildacenko@gmail.com>
2023-04-25 10:45:01 +0300

    Fix bug with change inventory amount of books

--- borrowings/views.py
+++ borrowings/views.py
@@ -42,6 +42,7 @@ class BorrowingViewSet(
             if serializer.is_valid():
                 serializer.save()
                 borrow.book.inventory += 1
+                borrow.book.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(

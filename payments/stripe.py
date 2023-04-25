03375423fe19d69f1a197d9f258eb0389d27f54b
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-04-25 12:28:29 +0300

    added file stipe with urls

---
+++ payments/stripe.py
@@ -0,0 +1,44 @@
+from datetime import date
+
+import stripe
+
+from django.conf import settings
+
+from borrowings.models import Borrowing
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def stripe_session(
+        borrowing: Borrowing,
+        url: str,
+        start_date: date,
+        end_date: date,
+        is_ok: bool,
+) -> stripe.checkout.Session:
+    have_to_pay = (end_date - start_date).days * borrowing.book.daily_fee
+    product = ""
+    if is_ok:
+        have_to_pay *= 2
+        product = "free "
+
+    url = url.rsplit("/", 2)[0] + "/borrowings/" + str(borrowing.id)
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "amount": int(have_to_pay * 100),
+                    "product_data": {
+                        "name": product + str(borrowing),
+                    },
+                },
+                "quantity":  1,
+            },
+        ],
+        mode="payment",
+        success_url=url + "/success?session_id={checkout_session.id}",
+        cancel_url=url + "/cancel?session_id={checkout_session.id}",
+    )
+
+    return checkout_session

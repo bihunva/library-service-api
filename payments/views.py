b16839a642cf4d3ab670f4178591bb9d89622ac8
oksana-feshchenko <foksanv@gmail.com>
2023-04-26 11:23:36 +0300

    Create success action to check, if paid

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,16 @@
-from rest_framework import viewsets, mixins
+import stripe
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from config import settings
 
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class PaymentViewSet(
@@ -21,3 +29,28 @@ class PaymentViewSet(
             return self.queryset.filter(borrowing__user=user)
 
         return self.queryset
+
+    @action(methods=["GET"], detail=True, url_path="success")
+    def success(self, request, pk):
+        payment = Payment.objects.get(pk=pk)
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        session_id = payment.session_id
+        events = stripe.Event.list(type="checkout.session.completed")
+        filtered_events = [
+            event
+            for event in events["data"]
+            if event["data"]["object"]["id"] == session_id
+            and event["type"] == "checkout.session.completed"
+        ]
+        if len(filtered_events) > 0:
+            event_data = filtered_events[0]["data"]["object"]
+            payment_status = event_data["payment_status"]
+            if payment_status == "paid":
+                payment.status = "PAID"
+                payment.save()
+                return HttpResponseRedirect(
+                    reverse(
+                        "payments:payment-detail", kwargs={"pk": payment.id}
+                    )
+                )
+        return HttpResponseRedirect(payment.session_url)
